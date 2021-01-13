ddc282002670e6b1b97eb4e0a9553c583427e9be
Alexandru-Cristian Balmus <alexbalmus@gmail.com>
2021-01-13 12:32:10 +0200

    Initial files for MakeCode project

---
+++ DevIntro_01_HelloWorld.ts
@@ -0,0 +1,18 @@
+// Hello World! Salutare Lume! :)
+
+// Acesta este un comentariu.
+
+/* Acesta este tot un comentariu, 
+   scris pe
+   mai multe 
+   linii. */
+
+// Comentariile au rol informativ.
+// Ele nu influențează în vreun fel programul.
+
+/* 
+   Urmează un apel (o invocare) a unei funcții (mai precis, a unei "metode" - detalii mai târziu)
+   predefinite, care afișează un mesaj folosind LED-urile 
+   de la MicroBit:
+ */ 
+basic.showString("SALUT!");
